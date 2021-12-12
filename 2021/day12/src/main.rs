db8093be1aa73ac6948986c0d93ca2045d2991c7
kryptn <kryptn@gmail.com>
2021-12-11 23:19:30 -0700

    remove initial idea

--- 2021/day12/src/main.rs
+++ 2021/day12/src/main.rs
@@ -3,32 +3,6 @@ use std::{collections::HashMap, fmt::Debug, rc::Rc, sync::Mutex};
 
 use advent::input_store;
 
-// a
-// b
-// c
-// d
-// e
-// f
-// g
-// h
-// i
-// j
-// k
-// l
-// m
-// n
-// o
-// p
-// q
-// r
-// s
-// t
-// u
-// v
-// w
-// x
-// y
-// z
 
 struct Cave {
     name: String,
