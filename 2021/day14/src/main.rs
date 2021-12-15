b9723960af40ee80664282df762e903bdc286833
kryptn <kryptn@gmail.com>
2021-12-14 17:55:48 -0700

    2021 day 14 part 2

--- 2021/day14/src/main.rs
+++ 2021/day14/src/main.rs
@@ -1,9 +1,4 @@
-use std::{
-    collections::HashMap,
-    fmt::Debug,
-    rc::Rc,
-    sync::{Mutex, MutexGuard},
-};
+use std::{collections::HashMap, fmt::Debug};
 
 use advent::input_store;
 use itertools::Itertools;
@@ -14,15 +9,9 @@ use nom::{
 
 #[derive(Debug)]
 struct Rule {
-    before: String,
-    after: String,
+    catalyst: String,
+    pairs: (String, String),
     element: String,
-}
-
-impl std::fmt::Display for Rule {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}{} -> {}", self.before, self.after, self.element)
-    }
 }
 
 fn parse_rule(input: &str) -> IResult<&str, Rule> {
@@ -31,12 +20,17 @@ fn parse_rule(input: &str) -> IResult<&str, Rule> {
     let (input, _) = tag(" -> ")(input)?;
     let (input, element) = take(1usize)(input)?;
 
+    let catalyst = format!("{}{}", before, after);
+    let left = format!("{}{}", before, element);
+    let right = format!("{}{}", element, after);
+    let element = element.to_string();
+
     Ok((
         input,
         Rule {
-            before: before.to_string(),
-            after: after.to_string(),
-            element: element.to_string(),
+            catalyst,
+            pairs: (left, right),
+            element,
         },
     ))
 }
@@ -48,225 +42,77 @@ impl From<&str> for Rule {
     }
 }
 
-struct Atom {
-    element: String,
-    before: Option<Rc<Mutex<Atom>>>,
-    after: Option<Rc<Mutex<Atom>>>,
-}
+fn insert(polymer: &mut HashMap<String, isize>, rules: &Vec<Rule>) {
+    let mut changes: HashMap<&String, isize> = HashMap::new();
 
-impl Debug for Atom {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let before = if let Some(b) = &self.before {
-            let lock = b.lock().unwrap();
-            Some(lock.element.clone())
-        } else {
-            None
-        };
+    for rule in rules {
+        let catalyst = polymer.get(&rule.catalyst).unwrap().clone();
 
-        let after = if let Some(a) = &self.after {
-            let lock = a.lock().unwrap();
-            Some(lock.element.clone())
-        } else {
-            None
-        };
+        *changes.entry(&rule.catalyst).or_default() -= catalyst;
+        *changes.entry(&rule.pairs.0).or_default() += catalyst;
+        *changes.entry(&rule.pairs.1).or_default() += catalyst;
+        *changes.entry(&rule.element).or_default() += catalyst;
+    }
 
-        f.debug_struct("Atom")
-            .field("element", &self.element)
-            .field("before", &before)
-            .field("after", &after)
-            .finish()
+    for (key, delta) in changes {
+        *polymer.get_mut(key).unwrap() += delta;
     }
 }
 
-impl std::fmt::Display for Atom {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut polymer = self.element.clone();
+fn get_score(polymer: &HashMap<String, isize>) -> isize {
+    let elements: HashMap<String, isize> = polymer
+        .iter()
+        .filter(|(k, v)| k.len() == 1 && *v > &0)
+        .map(|(k, v)| (k.clone(), v.clone()))
+        .collect();
 
-        let mut next = self.after.clone();
-
-        while next.is_some() {
-            let next_atom = next.unwrap();
-            let lock = next_atom.lock().unwrap();
-            polymer += &lock.element;
-            next = lock.after.clone();
-        }
-
-        write!(f, "{}", polymer)
-    }
-}
-
-impl Atom {
-    fn new(element: String) -> Self {
-        Self {
-            element,
-            before: None,
-            after: None,
-        }
-    }
-
-    fn check_rule(&self, rule: &Rule) -> bool {
-        match &self.before {
-            Some(before) => {
-                let lock = before.lock().unwrap();
-                lock.element == rule.before && self.element == rule.after
-            }
-            _ => false,
-        }
-    }
-
-    fn insert(&mut self, rule: &Rule) {
-        let before = self.before.clone();
-
-        match before {
-            Some(before) => {
-                let mut before_lock = before.lock().unwrap();
-                let mut new_atom = Atom::new(rule.element.clone());
-                new_atom.before = self.before.clone();
-                new_atom.after = before_lock.after.clone();
-
-                let new_atom = Rc::new(Mutex::new(new_atom));
-                before_lock.after = Some(new_atom.clone());
-                self.before = Some(new_atom.clone());
-            }
-            _ => unreachable!(),
-        }
-    }
-
-    fn to_vec(&self) -> Vec<Rc<Mutex<Self>>> {
-        let mut next = self.after.clone();
-        let mut out = Vec::new();
-
-        while next.is_some() {
-            let after = next.clone().unwrap();
-            out.push(after.clone());
-            let after_lock = after.lock().unwrap();
-            let after_after = after_lock.after.clone();
-            //out.push(after_after.clone());
-            next = after_after;
-        }
-
-        out
-    }
-
-    fn count_elements(&self) -> HashMap<String, u64> {
-        let mut out = HashMap::new();
-        let mut next = self.after.clone();
-
-        *out.entry(self.element.clone()).or_insert(0) += 1;
-
-        while next.is_some() {
-            let after = next.clone().unwrap();
-            let after_lock = after.lock().unwrap();
-            *out.entry(after_lock.element.clone()).or_insert(0) += 1;
-            let after_after = after_lock.after.clone();
-            //out.push(after_after.clone());
-            next = after_after;
-        }
-
-        out
-    }
-}
-
-fn init_polymer(given: &str) -> Vec<Rc<Mutex<Atom>>> {
-    let mut out = Vec::new();
-    for chr in given.trim().chars() {
-        let element = chr.to_string();
-        out.push(Rc::new(Mutex::new(Atom::new(element))));
-    }
-
-    for (a, b) in out.iter().tuple_windows() {
-        let ac = a.clone();
-        let bc = b.clone();
-
-        if let Ok(mut a_lock) = a.lock() {
-            a_lock.after = Some(bc);
-        }
-
-        if let Ok(mut b_lock) = b.lock() {
-            b_lock.before = Some(ac);
-        }
-    }
-
-    out
-}
-
-fn apply(first: Rc<Mutex<Atom>>, rules: &Vec<Rule>) {
-    {
-        let atoms = {
-            let f_lock = first.lock().unwrap();
-            f_lock.to_vec()
-        };
-
-        atoms.iter().for_each(|e| {
-            let mut lock = e.lock().unwrap();
-
-            for rule in rules {
-                if lock.check_rule(rule) {
-                    lock.insert(rule);
-                    break;
-                }
-            }
-        });
-    }
+    elements.values().max().unwrap() - elements.values().min().unwrap()
 }
 
 fn main() {
     let input = input_store::get_input(2021, 14);
 
-    //     let input = r#"NNCB
-
-    // CH -> B
-    // HH -> N
-    // CB -> H
-    // NH -> C
-    // HB -> C
-    // HC -> B
-    // HN -> C
-    // NN -> C
-    // BH -> H
-    // NC -> B
-    // NB -> B
-    // BN -> B
-    // BB -> N
-    // BC -> B
-    // CC -> N
-    // CN -> C"#;
-
     let mut input_split = input.trim().split("\n\n");
 
     let given = input_split.next().unwrap();
+
     let rules: Vec<Rule> = {
         let r = input_split.next().unwrap();
         r.lines().map(|l| l.into()).collect()
     };
 
-    let polymer = init_polymer(given);
-    // dbg!(&polymer);
-    // dbg!(&rules);
+    let mut polymer: HashMap<String, isize> =
+        rules.iter().map(|r| (r.catalyst.clone(), 0)).collect();
 
-    let first = { polymer.iter().take(1).next().unwrap().clone() };
-
-    for i in 0..10 {
-        println!("iteration {}", i);
-        apply(first.clone(), &rules);
+    for (l, r) in given.chars().tuple_windows() {
+        let pair: String = [l, r].iter().collect();
+        *polymer.get_mut(&pair).unwrap() += 1;
     }
-    let element_counts = first.lock().unwrap().count_elements();
 
-    let part_1 = element_counts.values().max().unwrap() - element_counts.values().min().unwrap();
+    for l in 'A'..='Z' {
+        polymer.insert(l.to_string(), 0);
+    }
 
-    dbg!(element_counts);
+    for l in given.chars() {
+        let element = l.to_string();
+        *polymer.get_mut(&element).unwrap() += 1;
+    }
+
+    for _ in 0..10 {
+        insert(&mut polymer, &rules);
+    }
+
+    let part_1 = get_score(&polymer);
 
     println!("part_1 => {}", part_1);
 
-    // for i in 0..30 {
-    //     println!("iteration {}", i + 10);
-    //     apply(first.clone(), &rules);
-    // }
-    // let element_counts = first.lock().unwrap().count_elements();
+    for _ in 0..30 {
+        insert(&mut polymer, &rules);
+    }
 
-    // let part_2 = element_counts.values().max().unwrap() - element_counts.values().min().unwrap();
+    let part_2 = get_score(&polymer);
 
-    println!("part_2 => {}", "part_2");
+    println!("part_2 => {}", part_2);
 }
 
 #[cfg(test)]
