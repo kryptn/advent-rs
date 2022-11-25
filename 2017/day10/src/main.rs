275192adeb1510998d2d3190b02a5fca37dc0473
kryptn <kryptn@gmail.com>
2022-11-24 22:52:57 -0700

    2017 day 10 part 2

--- 2017/day10/src/main.rs
+++ 2017/day10/src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
 use advent::input_store;
+use itertools::Itertools;
 
 fn step(position: usize, length: usize, ring: &mut Vec<usize>) {
     // dbg!(position, length, &ring);
@@ -12,6 +13,39 @@ fn step(position: usize, length: usize, ring: &mut Vec<usize>) {
     ring.rotate_right(position);
 }
 
+struct KnotHasher {
+    ring: Vec<usize>,
+    lengths: Vec<usize>,
+    position: usize,
+    skip: usize,
+}
+
+impl KnotHasher {
+    fn new(ring_size: usize, lengths: Vec<usize>) -> Self {
+        let ring = (0..ring_size).into_iter().collect();
+
+        Self {
+            ring,
+            lengths,
+            position: 0,
+            skip: 0,
+        }
+    }
+}
+
+impl Iterator for KnotHasher {
+    type Item = Vec<usize>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for length in self.lengths.iter() {
+            step(self.position, *length, &mut self.ring);
+            self.position = (self.position + length + self.skip) % self.ring.len();
+            self.skip += 1;
+        }
+        Some(self.ring.clone())
+    }
+}
+
 fn main() {
     let input = input_store::get_input(2017, 10);
     // let input = "3,4,1,5";
@@ -21,18 +55,33 @@ fn main() {
         .map(|d| d.parse().unwrap())
         .collect();
 
-    let mut ring: Vec<usize> = (0..256).into_iter().collect();
+    let mut hasher = KnotHasher::new(256, lengths);
+    let hashed = hasher.next().unwrap();
 
-    let mut position = 0;
-    let mut skip = 0;
-    for length in lengths {
-        step(position, length, &mut ring);
-        position = (position + length + skip) % ring.len();
-        skip += 1;
+    println!("part_1 => {}", hashed[0] * hashed[1]);
+
+    let lengths: Vec<usize> = input
+        .trim()
+        .as_bytes()
+        .iter()
+        .cloned()
+        .map(|b| b.into())
+        .chain(vec![17, 31, 73, 47, 23])
+        .collect();
+    let mut hasher = KnotHasher::new(256, lengths);
+    for _ in 0..64 {
+        hasher.next();
     }
 
-    println!("part_1 => {}", ring[0] * ring[1]);
-    println!("part_2 => {}", "not done");
+    let dense_hash: Vec<usize> = hasher
+        .ring
+        .chunks(16)
+        .map(|ch| ch.iter().cloned().reduce(|a, b| a ^ b).unwrap())
+        .collect();
+
+    let hex_string = dense_hash.iter().map(|ch| format!("{:02x}", ch)).join("");
+
+    println!("part_2 => {}", hex_string);
 }
 
 #[cfg(test)]
