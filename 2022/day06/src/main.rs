e403554840ca7edb44ba6c5d28bb22d4050dff59
kryptn <kryptn@gmail.com>
2022-12-06 21:56:00 -0700

    better name

--- 2022/day06/src/main.rs
+++ 2022/day06/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use advent::input_store;
 
-fn unique_windows(input: &str, window: usize) -> Option<usize> {
+fn first_unique_window(input: &str, window: usize) -> Option<usize> {
     for i in 0..input.len() - window {
         let set: HashSet<char> = input[i..i + window].chars().collect();
         if set.len() == window {
@@ -15,8 +15,8 @@ fn unique_windows(input: &str, window: usize) -> Option<usize> {
 fn main() {
     let input = input_store::get_input(2022, 06);
 
-    println!("part_1 => {}", unique_windows(&input, 4).unwrap());
-    println!("part_2 => {}", unique_windows(&input, 14).unwrap());
+    println!("part_1 => {}", first_unique_window(&input, 4).unwrap());
+    println!("part_2 => {}", first_unique_window(&input, 14).unwrap());
 }
 
 #[cfg(test)]
