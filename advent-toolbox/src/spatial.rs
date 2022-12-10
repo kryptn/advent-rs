68ae43609a4dc2bb4185e32a8bbb403d5f639704
kryptn <kryptn@gmail.com>
2022-12-09 19:47:53 -0700

    macro-impl'd coordinates

--- advent-toolbox/src/spatial.rs
+++ advent-toolbox/src/spatial.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, hash::Hash, ops::Add};
 
 trait Point {}
 
@@ -27,6 +27,45 @@ impl<P: Point, T> Space<P, T> {
     }
 }
 
+#[derive(Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
+struct Coordinate {
+    x: i32,
+    y: i32,
+}
+
+impl Coordinate {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Add<Coordinate> for Coordinate {
+    type Output = Self;
+
+    fn add(self, rhs: Coordinate) -> Self::Output {
+        ((self.x + rhs.x), (self.y + rhs.y)).into()
+    }
+}
+
+macro_rules! coord {
+    ( $x:ty ) => {
+        impl From<($x, $x)> for Coordinate {
+            fn from((x, y): ($x, $x)) -> Self {
+                let x = x as i32;
+                let y = y as i32;
+                Self { x, y }
+            }
+        }
+    };
+}
+
+coord!(usize);
+coord!(isize);
+coord!(u32);
+coord!(i32);
+
+impl Point for Coordinate {}
+
 impl Point for (i32, i32) {}
 
 impl<K: Point, V> FromIterator<(K, V)> for Space<K, V>
@@ -42,13 +81,28 @@ where
 
 #[cfg(test)]
 mod test {
-
     use super::*;
 
     #[test]
     fn create_grid() {
-        let objects: Vec<((i32, i32), bool)> = vec![((1, 1), false)];
+        let objects = vec![((2, 4), false)];
         let g: Space<(i32, i32), bool> = objects.into_iter().collect();
         dbg!(g);
+    }
+
+    #[test]
+    fn test_coordinate_intos() {
+        let expected = Coordinate::new(1, 1);
+        let c: Coordinate = (1 as usize, 1 as usize).into();
+        assert_eq!(c, expected);
+
+        let c: Coordinate = (1 as isize, 1 as isize).into();
+        assert_eq!(c, expected);
+
+        let c: Coordinate = (1 as u32, 1 as u32).into();
+        assert_eq!(c, expected);
+
+        let c: Coordinate = (1 as i32, 1 as i32).into();
+        assert_eq!(c, expected);
     }
 }
