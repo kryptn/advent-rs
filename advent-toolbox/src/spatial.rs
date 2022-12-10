7c9b70664007064e196697275c5a248d714638ad
kryptn <kryptn@gmail.com>
2022-12-09 19:17:44 -0700

    working generic space type?

--- advent-toolbox/src/spatial.rs
+++ advent-toolbox/src/spatial.rs
@@ -1,60 +1,54 @@
-// use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, hash::Hash};
 
-// trait Point {}
+trait Point {}
 
-// struct Space<P, T>(HashMap<P, T>)
-// where
-//     P: Point;
+#[derive(Debug)]
+struct Space<P, T>(HashMap<P, T>)
+where
+    P: Point;
 
-// impl<P: Point, T> std::ops::Deref for Space<P, T> {
-//     type Target = HashMap<P, T>;
+impl<P: Point, T> std::ops::Deref for Space<P, T> {
+    type Target = HashMap<P, T>;
 
-//     fn deref(&self) -> &Self::Target {
-//         &self.0
-//     }
-// }
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
-// impl<P: Point, T> std::ops::DerefMut for Space<P, T> {
-//     fn deref_mut(&mut self) -> &mut Self::Target {
-//         &mut self.0
-//     }
-// }
+impl<P: Point, T> std::ops::DerefMut for Space<P, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
 
-// impl<P: Point, T> Space<P, T> {
-//     fn new() -> Self {
-//         Space(HashMap::new())
-//     }
-// }
+impl<P: Point, T> Space<P, T> {
+    fn new() -> Self {
+        Space(HashMap::new())
+    }
+}
 
-// impl Point for (i32, i32) {}
+impl Point for (i32, i32) {}
 
-// // impl<P, T> From<&dyn FromIterator<(P, T)>> for Space<P, T>
-// // where
-// //     P: Point + Eq + Hash,
-// // {
-// //     fn from(item: &mut dyn Iterator<Item = (P, T)>) -> Self {
-// //         let inner = item.collect();
-// //         Self(inner)
-// //     }
-// // }
+impl<K: Point, V> FromIterator<(K, V)> for Space<K, V>
+where
+    K: Eq + Hash,
+{
+    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Space<K, V> {
+        let mut map = HashMap::with_hasher(Default::default());
+        map.extend(iter);
+        Self(map)
+    }
+}
 
-// impl<'a, P: 'a + 'static, T: 'a + 'static> FromIterator<&'static dyn Iterator<Item = (P, T)>> for Space<P, T>
-// where
-//     P: Point,
-// {
-//     fn from_iter<O: IntoIterator<Item = &'static dyn Iterator<Item = (P, T)>>>(iter: O) -> Self {
-//         todo!()
-//     }
-// }
+#[cfg(test)]
+mod test {
 
-// #[cfg(test)]
-// mod test {
+    use super::*;
 
-//     use super::*;
-
-//     #[test]
-//     fn create_grid() {
-//         let objects = vec![((1, 1), false)];
-//         let g: Space<(i32, i32), bool> = objects.collect();
-//     }
-// }
+    #[test]
+    fn create_grid() {
+        let objects: Vec<((i32, i32), bool)> = vec![((1, 1), false)];
+        let g: Space<(i32, i32), bool> = objects.into_iter().collect();
+        dbg!(g);
+    }
+}
