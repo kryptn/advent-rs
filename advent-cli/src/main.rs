2786fbc8ee1556fd89e7a93885542ca5c7db43a5
kryptn <kryptn@gmail.com>
2022-12-21 21:21:30 -0700

    added --wait to get and show

--- advent-cli/src/main.rs
+++ advent-cli/src/main.rs
@@ -1,10 +1,19 @@
+use std::{
+    io::{self, Write},
+    thread::sleep,
+    time::Duration,
+};
+
+use chrono::prelude::*;
+
 use advent::{
     fetch::{get_all_inputs, get_or_fetch_input},
-    input_store::{read_puzzle_input, set_cookie, Selector},
+    input_store::{set_cookie, Selector},
 };
 use clap::{command, Parser, Subcommand};
 
 use anyhow::Result;
+use humantime::format_duration;
 use rpassword::prompt_password;
 
 #[derive(Parser, Debug)]
@@ -17,37 +26,100 @@ struct Cli {
 #[derive(Subcommand, Debug)]
 enum Command {
     /// Get a specific day's input
-    Get { year: usize, day: usize },
+    Get {
+        year: usize,
+        day: usize,
+
+        /// waits until the puzzle is unlocked and then runs
+        #[clap(short, long, action, default_value_t = false, value_parser)]
+        wait: bool,
+    },
 
     /// Get the inputs for all of a year
     GetYear { year: usize },
 
     /// Show the input for a specific day
-    Show { year: usize, day: usize },
+    Show {
+        year: usize,
+        day: usize,
+
+        /// waits until the puzzle is unlocked and then runs
+        #[clap(short, long, action, default_value_t = false, value_parser)]
+        wait: bool,
+    },
 
     /// Set your adventofcode.com cookie
     SetCookie { cookie: Option<String> },
+}
+
+fn wait_for(selector: &Selector, force: bool) -> Result<String> {
+    let until = Utc
+        .with_ymd_and_hms(selector.year as i32, 12, (selector.day) as u32, 5, 0, 0)
+        .single()
+        .unwrap()
+        .naive_utc();
+
+    loop {
+        let now = Local::now();
+        let now_utc = now.naive_utc();
+        if now_utc < until {
+            let remaining = until - now_utc;
+            print!(
+                "  {now} ... waiting. {}              \r",
+                format_duration(remaining.to_std()?)
+            );
+            io::stdout().flush()?;
+            sleep(Duration::from_millis(100));
+        } else {
+            println!("{now} ... fetching");
+            break;
+        }
+    }
+
+    let mut attempts = 0;
+    let max_attempts = 5;
+
+    loop {
+        let result = get_or_fetch_input(&selector, force);
+        if result.is_ok() || attempts >= max_attempts {
+            return result;
+        } else {
+            println!("attempt {} failed", attempts + 1);
+            attempts += 1;
+            sleep(Duration::from_millis(attempts * 200));
+        }
+    }
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
-        Command::Get { year, day } => {
+        Command::Get { year, day, wait } => {
             let selector = Selector { year, day };
-            get_or_fetch_input(&selector, false)?;
+
+            if wait {
+                wait_for(&selector, false)?;
+            } else {
+                get_or_fetch_input(&selector, false)?;
+            }
+
             eprintln!("success: fetched {}-{:0>2}", year, day);
         }
         Command::GetYear { year } => {
             get_all_inputs(year, false)?;
             eprintln!("success: fetched all of year {}", year)
         }
-        Command::Show { year, day } => {
+        Command::Show { year, day, wait } => {
             let selector = Selector { year, day };
-            get_or_fetch_input(&selector, false)?;
-            if let Ok(value) = read_puzzle_input(&selector) {
-                println!("{}", value)
-            }
+
+            let value = if wait {
+                wait_for(&selector, false)?
+            } else {
+                get_or_fetch_input(&selector, false)?
+            };
+
+            println!("{value}");
         }
         Command::SetCookie { cookie } => {
             let cookie = match cookie {
