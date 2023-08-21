fa0b3b91a318dbd07e47ad7ec59e0ae6bd7d53d4
kryptn <kryptn@gmail.com>
2023-08-20 22:29:16 -0600

    2015 day 22 p1

--- 2015/day22/src/main.rs
+++ 2015/day22/src/main.rs
@@ -1,13 +1,27 @@
-use std::{cell::RefCell, rc::Rc, sync::Mutex};
+use std::{
+    cell::RefCell,
+    ops::Add,
+    rc::Rc,
+    sync::{Arc, Mutex},
+};
+
+use rayon::prelude::*;
 
 use advent::{fetch, numbers::factors};
 use itertools::{Combinations, Itertools, Powerset};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum Effect {
-    Shield(i32),
-    Poison(i32),
-    Recharge(i32),
+    Shield(u8),
+    Poison(u8),
+    Recharge(u8),
+}
+
+enum BattleStatus {
+    Culled(Battle),
+    Win(Battle),
+    Lose(Battle),
+    Continue(Battle),
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -39,368 +53,215 @@ static Shield: Spell = Spell {
 static Poison: Spell = Spell {
     mana_cost: 173,
     heal: 0,
-    damage: 4,
+    damage: 0,
     effect: Some(Effect::Poison(6)),
 };
 static Recharge: Spell = Spell {
     mana_cost: 229,
     heal: 0,
-    damage: 4,
+    damage: 0,
     effect: Some(Effect::Recharge(5)),
 };
+static BossAttack: Spell = Spell {
+    mana_cost: 0,
+    heal: 0,
+    damage: 9,
+    effect: None,
+};
 
-static Spells: [Spell; 5] = [MagicMissile, Drain, Shield, Poison, Recharge];
+static SPELLS: [&Spell; 5] = [&MagicMissile, &Drain, &Shield, &Poison, &Recharge];
 
-#[derive(Clone, Debug)]
-struct Entity {
-    health: i32,
-    mana: Option<i32>,
-    attack: Option<i32>,
+#[derive(Clone, Debug, Default)]
+struct Battle {
+    player_health: i32,
+    player_mana: i32,
+    player_mana_spent: i32,
+    boss_health: i32,
+    boss_attack: i32,
 
-    spent_mana: i32,
+    shield_turns: u8,
+    poison_turns: u8,
+    recharge_turns: u8,
 
-    active_effects: Vec<Effect>,
+    hurestic: Arc<RefCell<i32>>,
+    actions: Vec<&'static Spell>,
 }
 
-impl Entity {
-    fn apply_effect(&mut self, other: &mut Self, effect: Effect, indent: i32) -> Option<Effect> {
-        match effect {
-            Effect::Shield(turns) => {
-                println!("{}inner effect {:?}", indented("", indent), effect);
-                if turns <= 1 {
-                    None
-                } else {
-                    Some(Effect::Shield(turns - 1))
-                }
-            }
-            Effect::Poison(turns) => {
-                println!("{}inner effect {:?}", indented("", indent), effect);
-                other.health -= 3;
-                if turns <= 1 {
-                    None
-                } else {
-                    Some(Effect::Poison(turns - 1))
-                }
-            }
-            Effect::Recharge(turns) => {
-                println!("{}inner effect {:?}", indented("", indent), effect);
-                self.mana = Some(self.mana.unwrap() + 101);
-                if turns <= 1 {
-                    None
-                } else {
-                    Some(Effect::Recharge(turns - 1))
-                }
-            }
-        }
-    }
+impl Battle {
+    fn branches(&self) -> Vec<BattleStatus> {
+        let temp = self.with_effects();
 
-    fn apply_effects(&mut self, other: &mut Self, indent: i32) {
-        let mut next_round = Vec::new();
-        for effect in self.active_effects.clone() {
-            let next_effect = self.apply_effect(other, effect, indent);
-            if let Some(e) = next_effect {
-                next_round.push(e);
+        let status = temp.status();
+        let temp = match status {
+            BattleStatus::Culled(_) | BattleStatus::Win(_) | BattleStatus::Lose(_) => {
+                return vec![status]
             }
-        }
-
-        self.active_effects = next_round;
-    }
-
-    fn has_defense(&self) -> bool {
-        for effect in &self.active_effects {
-            if let Effect::Shield(_) = effect {
-                return true;
-            }
-        }
-        false
-    }
-
-    fn apply_damage(&mut self, damage: i32) {
-        let defense = if self.has_defense() { 7 } else { 0 };
-        let damage = {
-            let d = damage - defense;
-            if d <= 1 {
-                1
-            } else {
-                d
-            }
-        };
-        self.health -= damage;
-    }
-
-    fn valid_spells(&self, indent: i32) -> Vec<Spell> {
-        let mut out = Vec::new();
-
-        let mana = match self.mana {
-            Some(m) => m,
-            None => return out,
+            BattleStatus::Continue(battle) => battle,
         };
 
-        if mana > MagicMissile.mana_cost {
-            out.push(MagicMissile);
-        }
-        if mana > Drain.mana_cost {
-            out.push(Drain);
-        }
-        if mana > Shield.mana_cost
-            && !self
-                .active_effects
-                .iter()
-                .any(|e| matches!(e, Effect::Shield(_)))
-        {
-            out.push(Shield);
-        }
-        if mana > Poison.mana_cost
-            && !self
-                .active_effects
-                .iter()
-                .any(|e| matches!(e, Effect::Poison(_)))
-        {
-            out.push(Poison);
-        }
-        if mana > Recharge.mana_cost
-            && !self
-                .active_effects
-                .iter()
-                .any(|e| matches!(e, Effect::Recharge(_)))
-        {
-            out.push(Recharge);
+        // valid spells
+        let mut out = vec![];
+        for spell in SPELLS {
+            if temp.player_mana < spell.mana_cost {
+                continue;
+            }
+
+            if let Some(effect) = spell.effect {
+                if match effect {
+                    Effect::Shield(_) => temp.shield_turns > 0,
+                    Effect::Poison(_) => temp.poison_turns > 0,
+                    Effect::Recharge(_) => temp.recharge_turns > 0,
+                } {
+                    continue;
+                }
+            }
+
+            out.push(spell)
         }
 
-        println!("{}valid spells: {:?}", indented("", indent), out);
+        // if no spells, lose
+        if out.is_empty() {
+            return vec![BattleStatus::Lose(temp)];
+        }
+
+        out.into_iter().map(|spell| temp.with(&spell)).collect()
+    }
+
+    fn bound(&self) -> bool {
+        self.player_mana_spent < *self.hurestic.borrow()
+    }
+
+    fn status(self) -> BattleStatus {
+        if self.boss_health <= 0 {
+            println!(
+                "win! actions: {}, mana spent: {}",
+                self.actions.len(),
+                self.player_mana_spent
+            );
+            if *self.hurestic.borrow() > self.player_mana_spent {
+                let mut lowest = self.hurestic.borrow_mut();
+                *lowest = self.player_mana_spent;
+            }
+            BattleStatus::Win(self)
+        } else if self.player_health <= 0 {
+            BattleStatus::Lose(self)
+        } else if self.player_mana_spent > *self.hurestic.borrow() {
+            BattleStatus::Continue(self)
+        } else {
+            BattleStatus::Continue(self)
+        }
+    }
+
+    fn with_effects(&self) -> Self {
+        let mut out = self.clone();
+        if out.shield_turns > 0 {
+            out.shield_turns -= 1;
+        }
+        if out.poison_turns > 0 {
+            out.poison_turns -= 1;
+            out.boss_health -= 3;
+        }
+        if out.recharge_turns > 0 {
+            out.recharge_turns -= 1;
+            out.player_mana += 101;
+        }
 
         out
     }
 
-    fn use_spell(&mut self, other: &mut Entity, spell: Spell, indent: i32) {
-        self.mana = Some(self.mana.unwrap() - spell.mana_cost);
-        self.spent_mana += spell.mana_cost;
-        self.health += spell.heal;
-        other.health -= spell.damage;
-
-        println!("{}player casts spell", indented("", indent));
-        println!("{}{:?}", indented("", indent), self);
-        println!("{}{:?}", indented("", indent), spell);
-
+    fn with(&self, spell: &'static Spell) -> BattleStatus {
+        let mut out = self.clone();
+        out.player_mana -= spell.mana_cost;
+        out.player_mana_spent += spell.mana_cost;
+        out.boss_health -= spell.damage;
+        out.player_health += spell.heal;
+        out.actions.push(spell);
         if let Some(effect) = spell.effect {
-            println!("{}applied effect {:?}", indented("", indent), effect);
-
-            self.active_effects.push(effect);
-        }
-    }
-
-    // fn step(&mut self, other: &mut Entity) {
-    //     self.apply_effects(other);
-
-    //     for spell in self.valid_spells() {
-    //         self.use_spell(other, spell);
-    //     }
-    // }
-}
-
-#[derive(Clone, Debug)]
-enum Turn {
-    Player,
-    Enemy,
-}
-
-impl Turn {
-    fn other(&self) -> Self {
-        match self {
-            Turn::Player => Turn::Enemy,
-            Turn::Enemy => Turn::Player,
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-struct Battle {
-    player: Entity,
-    enemy: Entity,
-
-    turn: Turn,
-
-    lowest_mana_spent: Rc<RefCell<Option<i32>>>,
-}
-
-fn indented(msg: &str, indent: i32) -> String {
-    let mut ind = String::from("");
-    for _ in 0..indent {
-        ind.push_str("    ");
-    }
-
-    ind.push_str(msg);
-
-    ind
-}
-
-impl Battle {
-    fn new(player: Entity, enemy: Entity) -> Self {
-        let mana = Rc::new(RefCell::new(None));
-        Self {
-            player,
-            enemy,
-            turn: Turn::Player,
-            lowest_mana_spent: mana,
-        }
-    }
-
-    fn winner(&self, indent: i32) -> Option<Turn> {
-        let lowest_spent = match self.lowest_mana_spent.borrow().clone() {
-            Some(i) => i,
-            None => 10000,
-        };
-        if self.player.valid_spells(indent).len() == 0 && self.player.health <= 0
-            || self.player.spent_mana > lowest_spent
-        {
-            println!(
-                "{}returning player lost {:?}",
-                indented("", indent),
-                self.player
-            );
-            println!(
-                "{}                      {:?}",
-                indented("", indent),
-                self.enemy
-            );
-            println!(
-                "{}                      lowest: {:?}",
-                indented("", indent),
-                self.lowest_mana_spent
-            );
-
-            Some(Turn::Enemy)
-        } else if self.enemy.health <= 0 {
-            println!(
-                "{}returning player won {:?}",
-                indented("", indent),
-                self.player
-            );
-            println!(
-                "{}                     {:?}",
-                indented("", indent),
-                self.enemy
-            );
-            println!(
-                "{}                      lowest: {:?}",
-                indented("", indent),
-                self.lowest_mana_spent
-            );
-            Some(Turn::Player)
-        } else {
-            println!("{}no win  {:?}", indented("", indent), self.player);
-            println!("{}        {:?}", indented("", indent), self.enemy);
-            println!(
-                "{}                      lowest: {:?}",
-                indented("", indent),
-                self.lowest_mana_spent
-            );
-            None
-        }
-    }
-
-    fn apply_effects(&mut self, indent: i32) {
-        self.player.apply_effects(&mut self.enemy, indent);
-    }
-
-    fn handle_turn(&mut self, indent: i32) -> Vec<Option<i32>> {
-        println!("\n\n\n");
-        self.apply_effects(indent);
-        // let winner = self.winner();
-        // dbg!(winner);
-        match self.winner(indent) {
-            Some(ent) => {
-                return match ent {
-                    Turn::Player => {
-                        let lowest_spent = match self.lowest_mana_spent.borrow().clone() {
-                            Some(i) => i,
-                            None => 10000,
-                        };
-                        if self.player.spent_mana < lowest_spent {
-                            let mut opt = self.lowest_mana_spent.borrow_mut();
-                            opt.replace(self.player.spent_mana);
-                        }
-                        vec![Some(self.player.spent_mana)]
-                    }
-                    Turn::Enemy => vec![None],
-                }
+            match effect {
+                Effect::Shield(turns) => out.shield_turns = turns,
+                Effect::Poison(turns) => out.poison_turns = turns,
+                Effect::Recharge(turns) => out.recharge_turns = turns,
             }
-            None => {}
         }
 
-        let outcomes: Vec<Option<i32>> = match self.turn {
-            Turn::Player => {
-                //println!("matched player");
-                //self.player.step(&mut self.enemy);
-                let mut outcomes = Vec::new();
+        // player action done
+        // check for end
 
-                for spell in self.player.valid_spells(indent) {
-                    println!("\n{}turn{}", indented("", indent), indent);
-                    println!("{}will cast {:?}", indented("", indent), spell);
-                    let mut battle = self.clone();
-                    battle.player.use_spell(&mut battle.enemy, spell, indent);
-                    battle.turn = battle.turn.other();
-                    outcomes.extend(battle.handle_turn(indent + 1));
-                }
-                outcomes
-            }
-            Turn::Enemy => {
-                println!("\n{}turn{}", indented("", indent), indent);
-                println!("{}enemy does damage ", indented("", indent));
-                self.player.apply_damage(self.enemy.attack.unwrap());
-                let mut battle = self.clone();
-                battle.turn = self.turn.other();
-                battle.handle_turn(indent + 1)
-            }
+        let status = out.status();
+        out = match status {
+            BattleStatus::Continue(battle) => battle,
+            _ => return status,
         };
 
-        //dbg!(&outcomes);
+        out = out.with_effects();
+        let status = out.status();
+        out = match status {
+            BattleStatus::Continue(battle) => battle,
+            _ => return status,
+        };
 
-        outcomes
+        out.player_health -= {
+            let defense_mod = if out.shield_turns > 0 { 7 } else { 0 };
+            let boss_damage = Ord::max(1, out.boss_attack - defense_mod);
+            boss_damage
+        };
+
+        out.status()
+    }
+
+    fn next_layer(&self) -> impl Iterator<Item = Self> {
+        self.branches().into_iter().filter_map(|bs| match bs {
+            BattleStatus::Continue(b) => {
+                if b.bound() {
+                    Some(b)
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        })
+    }
+
+    fn solve(self) {
+        let mut layer = vec![self];
+        let mut iteration = 0;
+        while layer.len() > 0 {
+            println!("loop {}, layer len: {}", iteration, layer.len());
+            println!(
+                "    first boss health: {}, last boss health: {}",
+                layer[0].boss_health,
+                layer[layer.len() - 1].boss_health
+            );
+            iteration += 1;
+            let next_layer = layer
+                .into_iter()
+                .map(|b| b.next_layer())
+                .flatten()
+                .sorted_by(|a, b| a.boss_health.cmp(&b.boss_health))
+                .collect();
+            layer = next_layer;
+        }
     }
 }
 
 fn main() {
     let input = fetch::get_input(2015, 22);
 
-    let player = Entity {
-        health: 50,
-        mana: Some(500),
-        spent_mana: 0,
-        attack: None,
-        active_effects: Vec::new(),
+    let lowest_mana = Arc::new(RefCell::new(i32::MAX));
+
+    let battle = {
+        let mut b = Battle::default();
+        b.player_health = 50;
+        b.player_mana = 500;
+        b.boss_attack = 9;
+        b.boss_health = 51;
+        b.hurestic = lowest_mana.clone();
+        b
     };
-    let enemy = Entity {
-        health: 51,
-        mana: None,
-        spent_mana: 0,
-        attack: Some(9),
-        active_effects: Vec::new(),
-    };
-    // let player = Entity {
-    //     health: 10,
-    //     mana: Some(250),
-    //     spent_mana: 0,
-    //     attack: None,
-    //     active_effects: Vec::new(),
-    // };
-    // let enemy = Entity {
-    //     health: 14,
-    //     mana: None,
-    //     spent_mana: 0,
-    //     attack: Some(8),
-    //     active_effects: Vec::new(),
-    // };
 
-    let mut battle = Battle::new(player, enemy);
+    battle.solve();
 
-    let result = battle
-        .handle_turn(0)
-        .iter()
-        .map(|v| v.unwrap_or(10000))
-        .min();
-
-    dbg!(result);
+    dbg!(lowest_mana);
 }
 
 #[cfg(test)]
