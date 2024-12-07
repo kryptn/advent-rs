07e4ba0460129d29c14b3da55174f3f686e1d8f8
kryptn <kryptn@gmail.com>
2024-12-07 10:23:20 -0700

    part2

--- 2024/day07/src/main.rs
+++ 2024/day07/src/main.rs
@@ -1,11 +1,14 @@
 use advent::input_store;
+use itertools::Itertools;
 
 const YEAR: usize = 2024;
 const DAY: usize = 07;
 
+#[derive(Debug, Clone, Copy)]
 enum Operator {
     Add,
     Multiply,
+    Concatenate,
 }
 
 struct Problem {
@@ -28,24 +31,18 @@ impl From<&str> for Problem {
 }
 
 impl Problem {
-    fn find(&self) -> usize {
-        let two: usize = 2;
-        let t = two.pow(self.given.len() as u32 - 1);
-
-        for mut oper in 1..=t {
+    fn find(&self, operators: Vec<Operator>) -> usize {
+        for oper in std::iter::repeat(operators)
+            .take(self.given.len() - 1)
+            .multi_cartesian_product()
+        {
             let mut value = self.given[0];
 
-            // for each remaining given number
-            for i in 1..self.given.len() {
-
-                // get the right-most bit in the operator and shift
-                let operator = oper & 1;
-                oper >>= 1;
-
+            for (g, operator) in self.given.iter().skip(1).zip(oper.iter()) {
                 match operator {
-                    0 => value += self.given[i],
-                    1 => value *= self.given[i],
-                    _ => panic!("Invalid operator"),
+                    Operator::Add => value += g,
+                    Operator::Multiply => value *= g,
+                    Operator::Concatenate => value = format!("{}{}", value, g).parse().unwrap(),
                 }
             }
 
@@ -60,26 +57,40 @@ impl Problem {
 
 fn main() {
     let input = input_store::get_input(YEAR, DAY);
-//     let input = r#"190: 10 19
-// 3267: 81 40 27
-// 83: 17 5
-// 156: 15 6
-// 7290: 6 8 6 15
-// 161011: 16 10 13
-// 192: 17 8 14
-// 21037: 9 7 18 13
-// 292: 11 6 16 20
-// "#;
+    //     let input = r#"190: 10 19
+    // 3267: 81 40 27
+    // 83: 17 5
+    // 156: 15 6
+    // 7290: 6 8 6 15
+    // 161011: 16 10 13
+    // 192: 17 8 14
+    // 21037: 9 7 18 13
+    // 292: 11 6 16 20
+    // "#;
 
     let problems = input
         .lines()
         .map(|p| Problem::from(p))
         .collect::<Vec<Problem>>();
 
-    let part_1 = problems.iter().map(|p| p.find()).sum::<usize>();
+    let part_1 = problems
+        .iter()
+        .map(|p| p.find(vec![Operator::Add, Operator::Multiply]))
+        .sum::<usize>();
 
     println!("part_1 => {}", part_1);
-    println!("part_2 => {}", "not done");
+
+    let part_2 = problems
+        .iter()
+        .map(|p| {
+            p.find(vec![
+                Operator::Add,
+                Operator::Multiply,
+                Operator::Concatenate,
+            ])
+        })
+        .sum::<usize>();
+    println!("part_2 => {}", part_2);
 }
 
 #[cfg(test)]
