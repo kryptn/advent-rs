cc545f3eab137ee19be596710f851039cec77fc9
kryptn <kryptn@gmail.com>
2024-12-10 01:21:13 -0700

    day 10 part 2 with petgraph

--- 2024/day10/src/main.rs
+++ 2024/day10/src/main.rs
@@ -1,69 +1,82 @@
 use advent::input_store;
-use advent_toolbox::{algo::dijkstra_all_paths, spatial::{Coordinate, Space}};
+use advent_toolbox::spatial::{Coordinate, Space};
+use petgraph::prelude::DiGraphMap;
 
 const YEAR: usize = 2024;
 const DAY: usize = 10;
 
+fn do_thing_with_graph(outdoors: &Space<Coordinate, u32>) -> (usize, usize) {
+    let mut g: DiGraphMap<Coordinate, u32> = DiGraphMap::new();
 
-
-fn do_thing(outdoors: &Space<Coordinate, u32>) -> usize {
-    let outdoors = outdoors.clone();
-
-    let edges = |c: &Coordinate| c.cardinals().iter().filter(|n| {
-        match (outdoors.get(c), outdoors.get(n)) {
-            (Some(h), Some(nh)) => nh > h && nh-h == 1,
-            _ => false,
+    for (c, _) in outdoors.iter() {
+        let edges = c
+            .cardinals()
+            .iter()
+            .filter(|n| match (outdoors.get(c), outdoors.get(n)) {
+                (Some(h), Some(nh)) => nh > h && nh - h == 1,
+                _ => false,
+            })
+            .map(|&a| a)
+            .collect::<Vec<Coordinate>>();
+        g.add_node(*c);
+        for n in edges {
+            g.add_edge(*c, n, 1);
         }
-    }).map(|&a| a).collect();
-
-    let is_goal = |c: &Coordinate| -> bool {
-        match outdoors.get(c) {
-            Some(h) => *h == 9,
-            None => false,
-        }
-    };
-
-    let cost_fn = |c: &Coordinate| -> Option<usize> {
-        match outdoors.get(c) {
-            Some(h) => Some(*h as usize),
-            None => None,
-        }
-    };
-
-    let trailheads = outdoors.iter().filter(|(c, h)| **h == 0).map(|(c, _)| *c).collect::<Vec<Coordinate>>();
-    // dbg!(&trailheads);
-    let mut paths = vec![];
-
-    for trailhead in trailheads {
-        let path = dijkstra_all_paths(&[trailhead], edges, is_goal, Some(cost_fn));
-        paths.extend(path);
     }
 
-    paths.len()
-}
+    let trailheads: Vec<Coordinate> = outdoors
+        .iter()
+        .filter(|(_, h)| **h == 0)
+        .map(|(c, _)| *c)
+        .collect();
 
+    let mut part_1 = 0;
+    let mut part_2 = 0;
+
+    for trailhead in trailheads.iter() {
+        let path = petgraph::algo::dijkstra(&g, *trailhead, None, |(_, _, w)| *w);
+
+        let nines: Vec<Coordinate> = path
+            .iter()
+            .filter(|(_, h)| **h == 9)
+            .map(|(c, _)| *c)
+            .collect();
+        part_1 += nines.len();
+
+        for nine in nines {
+            part_2 += petgraph::algo::all_simple_paths::<
+                Vec<Coordinate>,
+                &DiGraphMap<Coordinate, u32>,
+            >(&g, *trailhead, nine, 0, None)
+            .count();
+        }
+    }
+
+    (part_1, part_2)
+}
 
 fn main() {
     let input = input_store::get_input(YEAR, DAY);
-//     let input = r#"89010123
-// 78121874
-// 87430965
-// 96549874
-// 45678903
-// 32019012
-// 01329801
-// 10456732
-// "#;
+    //     let input = r#"89010123
+    // 78121874
+    // 87430965
+    // 96549874
+    // 45678903
+    // 32019012
+    // 01329801
+    // 10456732
+    // "#;
 
     let outdoors: Space<Coordinate, char> = input.into();
-    let outdoors = outdoors.iter().map(|(c, h)| (*c, h.to_digit(10).unwrap())).collect();
+    let outdoors: Space<Coordinate, u32> = outdoors
+        .iter()
+        .map(|(c, h)| (*c, h.to_digit(10).unwrap()))
+        .collect();
 
-    // println!("{}", outdoors);
+    let answers = do_thing_with_graph(&outdoors);
 
-    let part_1 = do_thing(&outdoors);
-
-    println!("part_1 => {}", part_1);
-    println!("part_2 => {}", "not done");
+    println!("part_1 => {}", answers.0);
+    println!("part_2 => {}", answers.1);
 }
 
 #[cfg(test)]
